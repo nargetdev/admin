c89e5296ed34691e98802015e2a910979d22c147
Siddharth Suresh <sisuresh@umich.edu>
2014-09-14 14:27:11 -0400

    updating load_data

---
+++ sql/load_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO User (username,firstname,lastname, password, email) 
+VALUES('sportslover','Paul','Walker', 'password','sportslover@hotmail.com');
+
+INSERT INTO User (username,firstname,lastname, password, email) 
+VALUES('traveler','Rebecca','Travolta', 'password','rebt@explorer.org');
+
+INSERT INTO User (username,firstname,lastname, password, email) 
+VALUES('spacejunkie','Bob','Spacey', 'password','bspace@spacejunkies.net');
+
+INSERT INTO Album (albumid, title, created, lastupdated, username) 
+VALUES(0,'I love sports','2014-09-14', '2014-09-14','sportslover');
+
+INSERT INTO Album (albumid, title, created, lastupdated, username) 
+VALUES(1,'I love football','2014-09-14', '2014-09-14','sportslover');
+
+INSERT INTO Album (albumid, title, created, lastupdated, username) 
+VALUES(2,'Around The World','2014-09-14', '2014-09-14','traveler');
+
+INSERT INTO Album (albumid, title, created, lastupdated, username) 
+VALUES(3,'Cool Space Shots','2014-09-14', '2014-09-14','spacejunkie');
