7344af443c7fc0b0c8dbabf1f1a7a5d57725590a
Siddharth Suresh <sisuresh@umich.edu>
2014-09-14 16:08:15 -0400

    added world and football pic insert statements

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -18,3 +18,65 @@ VALUES(2,'Around The World','2014-09-14', '2014-09-14','traveler');
 
 INSERT INTO Album (albumid, title, created, lastupdated, username) 
 VALUES(3,'Cool Space Shots','2014-09-14', '2014-09-14','spacejunkie');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('football_s1','http://eecs485-05.eecs.umich.edu:5745/static/football_s1.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('football_s2','http://eecs485-05.eecs.umich.edu:5745/static/football_s2.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('football_s3','http://eecs485-05.eecs.umich.edu:5745/static/football_s3.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('football_s4','http://eecs485-05.eecs.umich.edu:5745/static/football_s4.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_EiffelTower','http://eecs485-05.eecs.umich.edu:5745/static/world_EiffelTower.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_firenze','http://eecs485-05.eecs.umich.edu:5745/static/world_firenze.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_GreatWall','http://eecs485-05.eecs.umich.edu:5745/static/world_GreatWall.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_Isfahan','http://eecs485-05.eecs.umich.edu:5745/static/world_Isfahan.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_Istanbul','http://eecs485-05.eecs.umich.edu:5745/static/world_Istanbul.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_Persepolis','http://eecs485-05.eecs.umich.edu:5745/static/world_Persepolis.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_Reykjavik','http://eecs485-05.eecs.umich.edu:5745/static/world_Reykjavik.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_Seoul','http://eecs485-05.eecs.umich.edu:5745/static/world_Seoul.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_Stonehenge','http://eecs485-05.eecs.umich.edu:5745/static/world_Isfahan.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_TajMahal','http://eecs485-05.eecs.umich.edu:5745/static/world_TajMahal.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_TelAviv','http://eecs485-05.eecs.umich.edu:5745/static/world_TelAviv.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_tokyo','http://eecs485-05.eecs.umich.edu:5745/static/world_tokyo.jpg','jpg','2012-01-05');
+
+INSERT INTO Photo (picid, url, format, date) 
+VALUES('world_WashingtonDC','http://eecs485-05.eecs.umich.edu:5745/static/world_WashingtonDC.jpg','jpg','2012-01-05');
+
+
+
+
+
+
+
+
+
+
+
